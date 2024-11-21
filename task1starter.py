d3eec9960007cbfadba7d2ecb44e093db886811b
AstridBrogren <astrid_brogren@biswashington.org>
2024-11-21 17:21:26 +0000

    task one

--- task1starter.py
+++ task1starter.py
@@ -3,4 +3,4 @@ number = int(input("Enter a number: "))
 
 # Use a for loop to print the multiplication table
 for i in range(1, 11):
-    # TODO: Print the result of number multiplied by i
+    print(i*number)
